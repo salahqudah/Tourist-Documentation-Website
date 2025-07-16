1b71f2eed238245efe18d7b859117f6bd804c4ea
salah qudah <salahqudah13@gmail.com>
2025-07-16 16:35:17 +0300

    edit some fixtures

--- places/models/tourist_place_model.py
+++ places/models/tourist_place_model.py
@@ -1,20 +1,30 @@
 from django.db import models
+import uuid
+from django.core.validators import MaxValueValidator, MinValueValidator
 from .tag_model import Tag
 
 class TouristPlace(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.CharField(max_length=500)
+    # UUID primary key
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
     city = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
     image = models.ImageField(upload_to='places/')
     opening_hours = models.CharField(max_length=255)
-    entry_fee = models.FloatField()
-    location = models.CharField(max_length=100, default='Not specified')
+    entry_fee = models.FloatField(validators=[MinValueValidator(0)], default=0)
+    location = models.CharField(max_length=255, default='Not specified')
     
-    # New field to track visits
+    # Track how many people visited the place
     visit_count = models.PositiveIntegerField(default=0)
 
-    # M2M with Tag
+    # Rating info (if needed)
+    total_rating = models.FloatField(validators=[MaxValueValidator(5)], default=0)
+    sum_of_ratings = models.IntegerField(validators=[MaxValueValidator(1000000)], default=0)
+    number_of_ratings = models.IntegerField(validators=[MaxValueValidator(1000000)], default=0)
+
+    # Many-to-many with tags
     tags = models.ManyToManyField(Tag, related_name='places', blank=True)
 
     def __str__(self):
